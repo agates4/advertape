51fb4b029c0a580a142ca59043651c7f947eaa4f
agates4 <agate97@gmail.com>
2017-09-11 03:33:19 -0400

    pretty reliable way to get the ad id

--- qr.php
+++ qr.php
@@ -7,20 +7,6 @@
         $dbopts = parse_url(getenv('DATABASE_URL'));
         return "dbname=".ltrim($dbopts["path"],'/')." host=".$dbopts["host"]." port=".$dbopts["port"]." user=".$dbopts["user"]." password=".$dbopts["pass"]." sslmode=require";
     }
-    
-    # Establish db connection
-    $db = pg_connect(pg_connection_string());
-    if (!$db) {
-        echo "Database connection error.";
-        exit;
-    }
-    
-    $result = pg_query($db, "SELECT * FROM ads");
-
-    while ($row = pg_fetch_row($result)) {
-        var_dump($row);
-        echo "<br />\n";
-    }
 
     echo "<br>";
 
@@ -28,6 +14,20 @@
         $decryptedCode = openssl_decrypt($_GET["code"], parse_url(getenv('ENCRYPT_METHOD'))["path"], parse_url(getenv('ENCRYPT_PASS'))["path"], false, parse_url(getenv('ENCRYPT_IV'))["path"]);
         $id = explode("&&&&", $decryptedCode)[1];
         echo $id;
+
+        # Establish db connection
+        $db = pg_connect(pg_connection_string());
+        if (!$db) {
+            echo "Database connection error.";
+            exit;
+        }
+        
+        $result = pg_query($db, "SELECT * FROM ads WHERE id = " . $id . ";");
+
+        while ($row = pg_fetch_row($result)) {
+            var_dump($row);
+            echo "<br />\n";
+        }
     }
     // var_dump(openssl_encrypt("Winner winner!&&&&2", parse_url(getenv('ENCRYPT_METHOD'))["path"], parse_url(getenv('ENCRYPT_PASS'))["path"], false, parse_url(getenv('ENCRYPT_IV'))["path"]));
 
