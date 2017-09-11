1764204f03f2dfbe160703b1311134e0bf28a381
agates4 <agate97@gmail.com>
2017-09-10 20:43:59 -0400

    database test

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+?>
+
 <html>
 
 <head>
