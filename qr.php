79191b0df4c13b15a72871933c7ac75e64964d8f
agates4 <agate97@gmail.com>
2017-09-11 03:39:45 -0400

    pretty reliable way to get the ad id

--- qr.php
+++ qr.php
@@ -1,6 +1,13 @@
 <?
     require __DIR__ . '/vendor/autoload.php';
 
+    $ENUM_ID = 0;
+    $ENUM_NAME = 1;
+    $ENUM_VALUE = 2;
+    $ENUM_CAMP_ID = 3;
+    $ENUM_USED = 4;
+    $ENUM_QR = 5;
+
     # This function reads your DATABASE_URL configuration automatically set by Heroku
     # the return value is a string that will work with pg_connect
     function pg_connection_string() {
@@ -8,12 +15,9 @@
         return "dbname=".ltrim($dbopts["path"],'/')." host=".$dbopts["host"]." port=".$dbopts["port"]." user=".$dbopts["user"]." password=".$dbopts["pass"]." sslmode=require";
     }
 
-    echo "<br>";
-
     if (isset($_GET["code"])) {
         $decryptedCode = openssl_decrypt($_GET["code"], parse_url(getenv('ENCRYPT_METHOD'))["path"], parse_url(getenv('ENCRYPT_PASS'))["path"], false, parse_url(getenv('ENCRYPT_IV'))["path"]);
         $id = explode("&&&&", $decryptedCode)[1];
-        echo $id;
 
         # Establish db connection
         $db = pg_connect(pg_connection_string());
@@ -23,11 +27,8 @@
         }
         
         $result = pg_query($db, "SELECT * FROM ads WHERE id = " . $id . ";");
-
-        while ($row = pg_fetch_row($result)) {
-            var_dump($row);
-            echo "<br />\n";
-        }
+        $row = pg_fetch_row($result)[0];
+        var_dump($row);
     }
     // var_dump(openssl_encrypt("Winner winner!&&&&2", parse_url(getenv('ENCRYPT_METHOD'))["path"], parse_url(getenv('ENCRYPT_PASS'))["path"], false, parse_url(getenv('ENCRYPT_IV'))["path"]));
 
